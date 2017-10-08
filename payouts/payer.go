495d11563851f74ee06873a3b1d26fbc4a67ba0c
pedrogms82 <pedrogms82@gmail.com>
2017-10-08 17:56:58 +0200

    Update payer.go

--- payouts/payer.go
+++ payouts/payer.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 
 	"github.com/pedrogms82/open-ethereum-pool/rpc"
 	"github.com/pedrogms82/open-ethereum-pool/storage"
