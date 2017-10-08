257c015fb4226b0be30fe170ccf1f59d1e9f98c7
pedrogms82 <pedrogms82@gmail.com>
2017-10-08 16:30:37 +0200

    Update main.go

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 
 	"github.com/yvasiyarov/gorelic"
 
-	"github.com/feeleep75/open-ethereum-pool/api"
-	"github.com/feeleep75/open-ethereum-pool/payouts"
-	"github.com/feeleep75/open-ethereum-pool/proxy"
-	"github.com/feeleep75/open-ethereum-pool/storage"
+	"github.com/pedrogms82/open-ethereum-pool/api"
+	"github.com/pedrogms82/open-ethereum-pool/payouts"
+	"github.com/pedrogms82/open-ethereum-pool/proxy"
+	"github.com/pedrogms82/open-ethereum-pool/storage"
 )
 
 var cfg proxy.Config
