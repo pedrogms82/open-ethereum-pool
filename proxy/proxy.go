1dfb095f7a7f78798be0cf7b5037bc12aafd69da
pedrogms82 <pedrogms82@gmail.com>
2017-10-08 17:03:01 +0200

    Update proxy.go

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -13,10 +13,10 @@ import (
 
 	"github.com/gorilla/mux"
 
-	"github.com/feeleep75/open-ethereum-pool/policy"
-	"github.com/feeleep75/open-ethereum-pool/rpc"
-	"github.com/feeleep75/open-ethereum-pool/storage"
-	"github.com/feeleep75/open-ethereum-pool/util"
+	"github.com/pedrogms82/open-ethereum-pool/policy"
+	"github.com/pedrogms82/open-ethereum-pool/rpc"
+	"github.com/pedrogms82/open-ethereum-pool/storage"
+	"github.com/pedrogms82/open-ethereum-pool/util"
 )
 
 type ProxyServer struct {
