d4a0bfc5c5c18242e644636fb50e9c43f4af5af3
pedrogms82 <pedrogms82@gmail.com>
2017-10-08 16:59:06 +0200

    Update blocks.go

--- proxy/blocks.go
+++ proxy/blocks.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 
-	"github.com/feeleep75/open-ethereum-pool/rpc"
-	"github.com/feeleep75/open-ethereum-pool/util"
+	"github.com/pedrogms82/open-ethereum-pool/rpc"
+	"github.com/pedrogms82/open-ethereum-pool/util"
 )
 
 const maxBacklog = 3
